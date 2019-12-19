dca742444648bfabf4253ba77106a2a5366804d2
wangshuo <89006176@sfmail.sf-express.com>
2019-12-19 10:20:18 +0800

    配置自定义扫描地址的方式
    Signed-off-by: wangshuo <89006176@sfmail.sf-express.com>

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -2,7 +2,11 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
+//配置扫描地址 的方式如下两种
+//@ComponentScan("com")  配置根目录
+//@ComponentScan({"com.example","com.check"})  配置多个目录
 @SpringBootApplication
 public class DemoApplication {
 
